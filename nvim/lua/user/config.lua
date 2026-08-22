96a0bda603b83514edfb0c7b244b2674ed14316f
Chiarandini <zak.katz123@gmail.com>
2026-08-22 01:12:12 -0400

    chore(nvim): turn the statusline filetype-profile marker back off

--- nvim/lua/user/config.lua
+++ nvim/lua/user/config.lua
@@ -3,7 +3,7 @@ return {
 	colorscheme = "gruvbox",
 	statusline = {
 		edge_style = "round", -- "round" | "slant" | "pointy" | "straight" | "bubbly"
-		filetype_profile = true, -- writing/code profile marker; click it for the live option values
+		filetype_profile = false, -- writing/code profile marker; click it for the live option values
 	},
 	obsidian_vault = "~/Documents/vault/",
 	completion_style = "supertab",
